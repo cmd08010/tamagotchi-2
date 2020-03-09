be6951db5e220816e433afe55de3fedc45ec50ec
cmd08010 <colleendunion@gmail.com>
2020-03-08 22:54:12 -0400

    test

--- db.js
+++ db.js
@@ -2,25 +2,77 @@ const pg = require("pg")
 const { Client } = pg
 const uuid = require("uuid/v4")
 const client = new Client(
-  process.env.DATABASE_URL || "postgres://localhost/acme_dictionary"
+  process.env.DATABASE_URL || "postgres://localhost/tamagotchi_db"
 )
 
 client.connect()
 
 const sync = async () => {
   const SQL = `    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-  DROP TABLE IF EXISTS table_name;
-  CREATE TABLE table_name
+  DROP TABLE IF EXISTS pet;
+  CREATE TABLE pet
   (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR NOT NULL,
+    hunger_level INT,
+    tired_level INT,
+    love_level INT,
+    image VARCHAR DEFAULT 'https://i.imgur.com/Y0q6OiD.jpg?1',
     date_create TIMESTAMP default CURRENT_TIMESTAMP
   );
-  INSERT INTO table_name (name) VALUES ('word');
+  INSERT INTO pet(name, hunger_level, tired_level, love_level) VALUES ('Woodley', '5','5','1');
+
+  INSERT INTO pet(name, hunger_level, tired_level, love_level, image) VALUES ('Bridges', '5','5','1', 'https://i.imgur.com/EzMJmh4.jpg?1');
+
+
   `
   await client.query(SQL)
 }
 
+const getPet = async () => {
+  const SQL = `
+  SELECT * FROM pet`
+  const response = await client.query(SQL)
+  return response.rows
+}
+
+const setLoveLevel = async id => {
+  const SQL = `
+  UPDATE pet SET love_level = love_level + 1 where id = $1
+  returning *`
+  const response = await client.query(SQL, [id])
+  return response.rows[0]
+}
+
+const setTiredLevel = async id => {
+  const SQL = ` UPDATE pet SET tired_level = tired_level + 1 where id = $1
+  returning *`
+  const response = await client.query(SQL, [id])
+
+  return response.rows[0]
+}
+
+const setHungerLevel = async id => {
+  const SQL = ` UPDATE pet SET hunger_level = hunger_level - 1 where id = $1
+  returning *
+  `
+  const response = await client.query(SQL, [id])
+  // console.log(response)
+  return response.rows[0]
+}
+
+const setName = async (id, name) => {
+  const SQL = `UPDATE pet SET name = $2 WHERE id= $1 returning *`
+  const response = await client.query(SQL, [id, name])
+  // console.log(response)
+  return response.rows[0]
+}
+
 module.exports = {
-  sync
+  sync,
+  getPet,
+  setHungerLevel,
+  setLoveLevel,
+  setTiredLevel,
+  setName
 }
--- server.js
+++ server.js
@@ -16,14 +16,45 @@ app.get("/", (req, res, next) => {
   res.sendFile(path.join(__dirname + "/index.html"))
 })
 
-app.post("/api/", (req, res, next) => {
+app.post("/api/pets", (req, res, next) => {
   console.log("wow")
 })
+
+app.get("/api/pets", (req, res, next) => {
+  db.getPet().then(response => {
+    console.log(response)
+    res.send(response)
+  })
+})
+
+app.get("/api/pets/:id", (req, res, next) => {
+  db.setName(req.params.id, req.body.name)
+})
+
+app.get("/api/users", (req, res, next) => {
+  console.log("test")
+  res.sendStatus(200)
+})
+
 app.delete("/api/", (req, res, next) => {
   console.log("wow")
 })
-app.put("/api/", (req, res, next) => {
-  console.log("wow")
+app.put("/api/pets/:action", (req, res, next) => {
+  if (req.params.action === "Feed") {
+    db.setHungerLevel(req.body.id).then(response => {
+      res.send(response)
+    })
+  }
+  if (req.params.action === "Play") {
+    db.setTiredLevel(req.body.id).then(response => {
+      res.send(response)
+    })
+  }
+  if (req.params.action === "Love") {
+    db.setLoveLevel(req.body.id).then(response => {
+      res.send(response)
+    })
+  }
 })
 
 db.sync()
